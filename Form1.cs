90cbb536a1bf61cef5bc0225dd527f4088489de8
Felipe Pescatori <felipepescatori@gmail.com>
2023-09-26 20:59:07 -0300

    w

--- Form1.cs
+++ Form1.cs
@@ -46,7 +46,7 @@ namespace WindowsFormsApp5
         {
             Articulos = new List<Articulos> { };
             {
-                new Articulos(77945678910111213, "Banana");
+                new Articulos(7790199603863, "Masitas");
                 new Articulos(77945678910111214, "Manzana");
             }
         }
